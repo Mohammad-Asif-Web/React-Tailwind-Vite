6206147730c9ba42ad1a8c89e00a1dcde07027c4
muhammad asif <alexasifhhossain@gmail.com>
2023-05-17 16:47:25 +0600

    conditional rendering added

--- src/Conditional_rendering/index.jsx
+++ src/Conditional_rendering/index.jsx
@@ -3,17 +3,30 @@ import Home from './Home'
 import Login from './Login'
 
 class index extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         
-      }
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       isLogin : true
     }
+  }
   render() {
+    // rendering by if-else statement
+    // let element;
+    // if(this.state.isLogin){
+    //   element = <Home />
+    // } else {
+    //   element = <Login />
+    // }
+    const {isLogin} = this.state
+
     return (
         <>
-            <div>This is index</div>
+          <div className='mt-5 text-3xl bg-slate-400 p-1'>Conditional Rendering</div>
+          {/* if else statement */}
+          {/* <div>{element}</div> */}
+
+          <div>{isLogin ? <Home /> : <Login />}</div>
         </>
     )
   }
