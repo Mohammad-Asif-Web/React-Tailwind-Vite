d3debcd5f99c2b46b46b2e2315b64bd87de9ded9
muhammad asif <alexasifhhossain@gmail.com>
2023-05-20 14:32:44 +0600

    useState with object and get form data in short code

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -3,21 +3,48 @@ import style from './form.module.css'
 
 function Form() {
     // Hooks can be called inside of the function body, otherwise it will error
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    // useState with object, this is best practice for multiple form values 
+    const [user, setUser] = useState({
+        name : '',
+        email : '',
+        password : ''
+    })
+    // Destructuring the name, email, password
+    const {name, email, password} = user
 
-    const handleName = (e)=>{
-        // console.log(e.target.value)
-        setName(e.target.value);
-    }
-    const handleEmail = (e)=>{
-        // console.log(e.target.value)
-        setEmail(e.target.value);
-    }
-    const handlePassword = (e)=>{
-        // console.log(e.target.value)
-        setPassword(e.target.value);
+    const handleEvent = (e)=>{
+
+        // METHOD 1 FOR GETTING MULTIPLE VALUE FORM INPUT FIELD
+        // It will get all the value automatic from input field, don't need to add statement specificly. this is best for professionals. short code, short time with spread operator
+        setUser({
+            ...user, // spread operator used to get all the input field data automatic
+            [e.target.name]: e.target.value // it will get input type name and it's value
+        })
+
+        // METHOD 2 FOR GETTING MULTIPLE VALUE FROM INPUT FIELD
+        // it will get the only value which statement we will add
+        // const inputName = e.target.name;
+        // if(inputName === 'name'){
+        //     setUser({
+        //         name : e.target.value,
+        //         email,
+        //         password
+        //     })
+        // }
+        // if(inputName === 'email'){
+        //     setUser({
+        //         name,
+        //         email : e.target.value,
+        //         password
+        //     })
+        // }
+        // if(inputName === 'password'){
+        //     setUser({
+        //         name,
+        //         email,
+        //         password : e.target.value
+        //     })
+        // }
     }
     const handleSubmit = (e)=>{
         // by submitting the form default it will erase all the value form input field. That's why we used e.preventDefault function so that the data of form can not be disappear
@@ -29,8 +56,6 @@ function Form() {
             userPass : password
         }
         console.log(userInfo);
-        // console.log(name, email, password);
-        // console.log("form is submitted");
     }
 
   return (
@@ -38,15 +63,15 @@ function Form() {
         <form action="" onSubmit={handleSubmit}>
             <div className={style.formGroup}>
                 <label htmlFor="name">Name: </label>
-                <input type="text" name='name' id='name' className='' onChange={handleName} value={name} />
+                <input type="text" name='name' id='name' className='' onChange={handleEvent} value={name} />
             </div>
             <div className={style.formGroup}>
                 <label htmlFor="email">Email: </label>
-                <input type="email" name='email' id='email' onChange={handleEmail} value={email} />
+                <input type="email" name='email' id='email' onChange={handleEvent} value={email} />
             </div>
             <div className={style.formGroup}>
                 <label htmlFor="password">Password: </label>
-                <input type="password" name='password' id='password' onChange={handlePassword} value={password} />
+                <input type="password" name='password' id='password' onChange={handleEvent} value={password} />
             </div>
             <input type="submit" value='Submit' className='border-2 px-4 py-1 bg-red-400 cursor-pointer' />
         </form>
